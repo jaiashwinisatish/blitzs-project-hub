120dea13ff4af11f0efcfd090b3d347b64357470
Xenonesis <ffjawed@gmail.com>
2026-01-29 23:06:56 +0530

    Add particles background using @tsparticles/react for animated background effect

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
+import ParticlesBackground from '../ui/ParticlesBackground';
 
 export const MainLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col relative">
+      <ParticlesBackground />
       <Navbar />
       <main className="flex-1 pt-16">
         <Outlet />
